72be6a9826c052d768e56df36c6dd8e9206a66c9
Brandon Cheah <brandoncheah@Brandons-MacBook-Pro.local>
2020-08-27 15:24:10 +0800

    test with algo

--- ai.py
+++ ai.py
@@ -1,5 +1,6 @@
 import random
 import math
+import threading
 
 class TicTacToe:
 	def __init__(self):
@@ -67,60 +68,94 @@ class TicTacToe:
 				return 1
 		return 0
 
-def heusomething_value(game):
-	N = visits.get("total", 1)
-	Ni = visits.get(hash(tuple(game.state())), 1e-5)
-	V = differential.get(hash(tuple(game.state())), 0) * 1.0 / Ni
-	return V + 0.3 * math.sqrt(math.log(N) / Ni)
+class AI():
+	def __init__(self):
+		self.visits = {}
+		self.differential = {}
 
-def record(game, score):
-	visits["total"] = visits.get("total", 1) + 1
-	visits[hash(tuple(game.state()))] = visits.get(hash(tuple(game.state())), 0) + 1
-	differential[hash(tuple(game.state()))] = differential.get(hash(tuple(game.state())), 0) + score
+	def heusomething_value(self, game):
+		N = self.visits.get("total", 1)
+		Ni = self.visits.get(str(game.state()), 1e-5)
+		V = self.differential.get(str(game.state()), 0) * 1.0 / Ni
+		return V + 100 * math.sqrt(math.log(N) / Ni)
 
-def simulate(game):
-	if game.over():
-		record(game, -game.score())
-		return -game.score()
+	def record(self, game, score):
+		self.visits["total"] = self.visits.get("total", 1) + 1
+		self.visits[str(game.state())] = self.visits.get(str(game.state()), 0) + 1
+		self.differential[str(game.state())] = self.differential.get(str(game.state()), 0) + score
 
-	action_heusomething = {}
-	for move in game.valid_moves():
+	def simulate(self, game, steps=math.inf):
+		if steps == 0 or game.over():
+			self.record(game, -game.score())
+			return -game.score()
+
+		action_heusomething = {}
+		for move in game.valid_moves():
+			game.move(move)
+			action_heusomething[move] = -self.heusomething_value(game)
+			game.undo()
+
+		move = max(action_heusomething, key=action_heusomething.get)
 		game.move(move)
-		action_heusomething[move] = -heusomething_value(game)
+		score = -self.simulate(game, steps - 1)
 		game.undo()
+		self.record(game, score)
 
-	move = max(action_heusomething, key=action_heusomething.get)
-	game.move(move)
-	score = -simulate(game)
-	game.undo()
-	record(game, score)
+		return score
 
-	return -score
+	def replay_score(self, game, N=100):
+		scores = [self.simulate(game) for i in range(0, N)]
+		return sum(scores) / len(scores)
 
-def replay_score(game, N=100):
-	scores = [simulate(game) for i in range(0, N)]
-	return sum(scores) / len(scores)
+	def ai_move(self, game):
+		actions = {}
+		for move in game.valid_moves():
+			game.move(move)
+			actions[move] = -self.replay_score(game)
+			game.undo()
+		return max(actions, key=actions.get)
 
-def ai_move(game):
-	actions = {}
-	for move in game.valid_moves():
-		game.move(move)
-		actions[move] = replay_score(game)
-		game.undo()
-	return max(actions, key=actions.get)
+wins = {"one": 0, "two": 0}
 
-visits = {}
-differential = {}
+ai1 = AI()
+ai2 = AI()
 
-for _ in range(0, 100):
-	game = TicTacToe()
-	while not game.over():
-		print(game)
-		move = random.choice(game.valid_moves())
-		if game.move(move) and not game.over():
-			move2 = ai_move(game)
-			game.move(move2)
-	print(game)
+def one_game(first, second):
+	for i in range(0, 5):
+		game = TicTacToe()
+		while not game.over():
+			# print(game)
+			move = first.ai_move(game)
+			# move = random.choice(game.valid_moves())
+			if game.move(move) and not game.over():
+				move2 = second.ai_move(game)
+				game.move(move2)
+		if game.score() == 1:
+			if len(game.moves) % 2 == 0:
+				wins["two"] += 1
+			else:
+				wins["one"] += 1
+		print(f"""
+{game}\n
+Player one: {wins['one']}
+Player two: {wins['two']}
+	""")
+
+one_game(ai1, ai2)
+
+# threads = []
+
+# for i in range(0, 100):
+# 	first = ai1 if i % 2 == 0 else ai2
+# 	second = ai2 if i % 2 == 0 else ai1
+# 	t = threading.Thread(target=one_game,args=(first, second))
+# 	threads += [t]
+# 	t.start()
+
+# print("Started all threads, joining...")
+
+# for t in threads:
+# 	t.join()
 
 while True:
 	game = TicTacToe()
@@ -128,10 +163,10 @@ while True:
 		print(game)
 		move = int(input("Give number: "))
 		if game.move(move) and not game.over():
-			move2 = ai_move(game)
+			move2 = ai2.ai_move(game)
 			game.move(move2)
 	print(game)
-	breakpoint()
+	# breakpoint()
 
 # hash(frozenset({"asd": "qwe"}.items()))
 
